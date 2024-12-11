6a3677210a42e8f03cee57c81e838dc0f31f40a4
Thalyson Luiz <thalysonluiz@gmail.com>
2024-12-11 10:53:44 -0300

    feat: create Welcome component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from '@/components/Welcome'
 import { View, Text } from 'react-native'
 
 export default function Index() {
   return (
-    <View>
-      <Text>Teste</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   )
 }
