bc68cbb4ccb006c026eacfc46de491d7f8dc9268
Thalyson Luiz <thalysonluiz@gmail.com>
2024-12-11 10:03:04 -0300

    chore: config fonts rubik

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,25 @@
-import { colors } from '@/styles/theme'
 import { Stack } from 'expo-router'
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold
+} from '@expo-google-fonts/rubik'
+import { colors } from '@/styles/theme'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+
   return (
     <Stack
       screenOptions={{
